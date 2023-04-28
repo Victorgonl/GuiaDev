fc55210fbc0db46e13b5f94045e015c995d9872e
Fabio Furtado <fabio.furtado@youxgroup.com.br>
2023-04-28 13:47:19 -0300

    commit

--- sender/views.py
+++ sender/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    
+    if request.GET.get('mytest'):
+        print('OK')
     return render(request,'index.html')
