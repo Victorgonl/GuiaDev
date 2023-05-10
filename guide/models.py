e16d747d9fa32a92ceca1a3b9bacf1efb21f231c
Fábio Tavares <74937496+fabioVitorTavares@users.noreply.github.com>
2023-05-09 21:08:58 -0300

    Update models.py

--- guide/models.py
+++ guide/models.py
@@ -1,10 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Colaborador(models.Model):
+  id = models.AutoField(primary_key=True)
+  nome = models.CharField(max_length=20)
+  email = models.CharField(max_length=20)
+  total_contribuicoes = models.IntegerField()
+
+  def __str__(self):
+    return 'Colaborador'
+    
+
+class Tecnologia(models.Model):
+  id = models.AutoField(primary_key=True)
+  nome = models.CharField(max_length=20)
+  descricao = models.CharField(max_length=500)
+
+  def __str__(self):
+    return 'Tecnologia'
 
 class Guia(models.Model):
-  guia_id = models.AutoField(primary_key=True)
-  guia_titulo = models.CharField(max_length=20)
-  guia_tecnologia = models.CharField(max_length=20)
-  guia_descricao = models.CharField(max_length=500)
-  guia_codigo = models.CharField(max_length=500)
+  id = models.AutoField(primary_key=True)
+  titulo = models.CharField(max_length=20)
+  descricao = models.CharField(max_length=500)
+  tecnologia = models.ForeignKey(Tecnologia, on_delete=models.RESTRICT)
+  codigo = models.CharField(max_length=500)
+  colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return 'Guia'
+  
+class Tutorial(models.Model):
+  id = models.AutoField(primary_key=True)
+  titulo = models.CharField(max_length=20)
+  tecnologia = models.ForeignKey(Tecnologia, on_delete=models.RESTRICT)
+  descricao = models.CharField(max_length=500)
+  codigo = models.CharField(max_length=500)
+  colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return 'Tutorial'
+  
+class Ranking(models.Model):
+  id = models.AutoField(primary_key=True)
+  colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE)
+  tecnologia = models.ForeignKey(Tecnologia, on_delete=models.RESTRICT)
+  total_contribuicoes = models.IntegerField()
+
+  def __str__(self):
+    return 'Ranking'
