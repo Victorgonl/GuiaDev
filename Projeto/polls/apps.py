dd3c8eeb77adb559f6c7f7095d139fea6c881dc8
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-05-09 22:33:47 -0300

    commit

--- Projeto/polls/apps.py
+++ Projeto/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GuideConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'guide'
+    name = 'polls'
