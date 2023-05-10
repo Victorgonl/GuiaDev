342ddd29ca259f0177bf3a83b22b02d39b595078
Fábio Tavares <74937496+fabioVitorTavares@users.noreply.github.com>
2023-05-09 21:14:39 -0300

    Update admin.py

--- guide/admin.py
+++ guide/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Guia
+from .models import Colaborador
+from .models import Ranking
+from .models import Tecnologia
+from .models import Tutorial
+
+admin.site.register(Guia)
+admin.site.register(Colaborador)
+admin.site.register(Ranking)
+admin.site.register(Tecnologia)
+admin.site.register(Tutorial)
+
