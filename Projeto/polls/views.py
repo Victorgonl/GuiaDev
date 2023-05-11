408cd4ade60eec55b1e827f406e5fa5d1f76d07e
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-05-10 22:48:24 -0300

    commit

--- Projeto/polls/views.py
+++ Projeto/polls/views.py
@@ -7,17 +7,35 @@ from django.http import HttpResponse
 from .models import Tecnologia
 
 
-def index(request):
-  tecnologias = Tecnologia.objects.all()
-
-  context = {
-    'tecnologias' : tecnologias
-  }
-
-  return render(request, 'index.html', context=context)
 
 def guias(request):
     return render(request, 'guias.html')
 
 def tutoriais(request):
     return render(request, 'tutoriais.html')
+
+
+
+    
+
+def index(request):
+  tecnologias = Tecnologia.objects.all()
+  palavra = ''
+
+  if request.method=='POST':
+    palavra = request.POST['palavra']
+    tecnologias_filtradas = []
+    for tec in tecnologias:
+      if (tec.nome.upper().find(palavra.upper()) == 0):
+        tecnologias_filtradas.append(tec)
+        print(tec.nome.upper(), palavra.upper())
+    
+  context = {
+    'tecnologias' : tecnologias_filtradas
+  }
+
+  return render(request, 'index.html', context=context)
+
+
+
+  
