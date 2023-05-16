20f8f308f9ca57b889c2920b8ca19aef44b13d91
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-05-16 00:15:34 -0300

    commit

--- Projeto/polls/forms.py
+++ Projeto/polls/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 class FormTutorial(forms.Form):
-  id = forms.CharField(max_length=500)
+  id = forms.CharField(max_length=20, required=False)
   like = forms.CharField(required=False)
   copy = forms.CharField(required=False)
+  pesquisa = forms.CharField(max_length=500, required=False)
   
--- Projeto/polls/views.py
+++ Projeto/polls/views.py
@@ -7,36 +7,38 @@ from .models import Tecnologia, Tutorial, Comentario
 
 
 def index(request):
+  
+
+  if request.method == 'POST':
+    form = FormTutorial(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data
+
+      id = data.get('id')
+      if bool(id):
+        return tutorial(request)
+      
+      pesquisa = data.get('pesquisa')
+      print(pesquisa)
+      tutoriais = Tutorial.objects.all()
+      tutoriais_filtrados = []
+      for tut in tutoriais:
+        if (pesquisa.upper() in tut.titulo.upper()):
+          tutoriais_filtrados.append(tut)
+      
+      context = {
+        'tutoriais': tutoriais_filtrados
+      }
+      return render(request, 'index.html', context=context)
+  else: 
     tutoriais = Tutorial.objects.all()
-    palavra = ''
-    tutoriais_filtrados = []
-
-    if request.method == 'POST':
-        data = request.POST
-        keys = ''.join(list(data.keys()))
-        if (keys.find('id') != -1):
-            return tutorial(request)
-
-        if (keys.find('palavra') != -1):
-            palavra = request.POST['palavra']
-            for tut in tutoriais:
-                if (tut.titulo.upper().find(palavra.upper()) == 0):
-                    tutoriais_filtrados.append(tut)
-            context = {
-                'tutoriais': tutoriais_filtrados
-            }
-            return render(request, 'index.html', context=context)
-
     context = {
-        'tutoriais': tutoriais
+      'tutoriais': tutoriais
     }
-
     return render(request, 'index.html', context=context)
-
 
 def tutorial(request):
 
-  tutorial = ''
   if request.method == 'POST':
     form = FormTutorial(request.POST)
     if form.is_valid():
@@ -44,54 +46,23 @@ def tutorial(request):
       id = data.get('id')
       like =  bool(data.get('like'))
       copy = bool(data.get('copy'))
-      
 
-      tutorial = Tutorial.objects.filter(id=id)
+      tutorial = Tutorial.objects.get(id=id)
         
-      if (like):
-        tutorial[0].total_likes += 1
-        tutorial[0].save()
+      if like:
+        likes = tutorial.total_likes
+        tutorial.__setattr__('total_likes', 1+likes)
+        tutorial.save()
 
       if copy:
-        print('copying')
-        pc.copy(tutorial[0].codigo)
+        pc.copy(tutorial.codigo)
 
-    tutorial[0].total_likes += 1
-    tutorial[0].save()
-    print(tutorial[0].total_likes)
-    print(tutorial[0].__getstate__())
-    context = {
-      'tutorial': tutorial,
-      'id': id
-    } 
-    return render(request, 'tutorial.html', context=context)
+      comentarios = Comentario.objects.filter(tutorial_id=id)
+      context = {
+        'comentarios': comentarios,
+        'tutorial': [tutorial],
+        'id': id
+      } 
+      return render(request, 'tutorial.html', context=context)
 
-    # if request.method == 'POST':
-    #     data = request.POST
-    #     keys = ''.join(list(data.keys()))
-    #     print(keys)
-    #     # print(request.POST)['like']
-    #     id = 1
-    #     if (keys.find('id') != -1):
-    #         id = request.POST['id']
-
-    #     tutorial = Tutorial.objects.filter(id=id)
-    #     todosComentarios = Comentario.objects.all()
-    #     comentarios = []
-    #     for com in todosComentarios:
-    #         if int(com.tutorial.id) == int(id):
-    #             comentarios.append(com)
-
-    #     if (keys.find('like') != -1):
-    #         tutorial[0].total_likes += 1
-    #         tutorial[0].save()
-
-    #     if (keys.find('copy') != -1):
-    #         pc.copy(tutorial[0].codigo)
-
-    #     context = {
-    #         'comentarios': comentarios,
-    #         'tutorial': tutorial,
-    #         'id': id
-    #     }
-    #     return render(request, 'tutorial.html', context=context)
+    
