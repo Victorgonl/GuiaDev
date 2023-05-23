f96ac99d6e3d46c03c3396aa67b9c232201e3143
victorgonl <victorgonl@outlook.com>
2023-05-23 12:04:44 -0300

    Informações úteis para o ambiente

--- .vscode/.venv.sh
+++ .vscode/.venv.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 if [ -f ".venv/bin/django-admin" ]; then
-    cd src; exec bash
+    echo "Execução do servidor: python3 manage.py runserver"
+    echo "Atualizar banco de dados: python3 manage.py makemigrations && python3 manage.py migrate"
+    cd src
+    exec bash
 else
-    echo "Comandos para criação do ambiente:\npython3 -m venv .venv\nsource .venv/bin/activate\npip install -r requirements.txt\ncd src"; exec bash
+    echo "Comandos para criação do ambiente:"
+    echo "python3 -m venv .venv"
+    echo "source .venv/bin/activate"
+    echo "pip install -r requirements.txt"
+    echo "cd src"
+    exec bash
 fi
