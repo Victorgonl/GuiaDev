0af09d80ae4f781f58a4da295eac628e608349a7
victorgonl <victorgonl@outlook.com>
2023-06-06 18:13:09 -0300

    Correção do nome dos diretórios

--- .vscode/.venv.sh
+++ .vscode/.venv.sh
@@ -3,13 +3,13 @@
 if [ -f ".venv/bin/django-admin" ]; then
     echo "Execução do servidor: python3 manage.py runserver"
     echo "Atualizar banco de dados: python3 manage.py makemigrations && python3 manage.py migrate"
-    cd guidedev
+    cd guiadev
     exec bash
 else
     echo "Comandos para criação do ambiente:"
     echo "python3 -m venv .venv"
     echo "source .venv/bin/activate"
     echo "pip install -r requirements.txt"
-    echo "cd guidedev"
+    echo "cd guiadev"
     exec bash
 fi
