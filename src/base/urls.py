fe3f8ddca081481d83f0bf7586b6c959ad408442
victorgonl <victorgonl@outlook.com>
2023-06-06 12:03:21 -0300

    Mudança no nome das views

--- src/base/urls.py
+++ src/base/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index"),
-    path("tutorial/", views.tutorial, name="tutorial"),
-    path("adicionar_tutorial/", views.adicionar_tutorial, name="adicionar_tutorial"),
-    path("login/", views.loginView, name="login"),
-    path("", views.inicio, name="inicio"),
-    path("cadastrar/", views.cadastrar, name="cadastrar"),
+    path("index/", views.index_view, name="index"),
+    path("tutorial/", views.tutorial_view, name="tutorial"),
+    path("adicionar_tutorial/", views.adicionar_tutorial_view, name="adicionar_tutorial"),
+    path("login/", views.login_view, name="login"),
+    path("", views.inicio_view, name="inicio"),
+    path("cadastrar/", views.cadastrar_view, name="cadastrar"),
 ]
