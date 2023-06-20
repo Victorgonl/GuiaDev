b5f25f2919282b91d260ff279bb1e1b1347928de
victorgonl <victorgonl@outlook.com>
2023-06-20 11:06:01 -0300

    Limpeza

--- guiadev/base/urls.py
+++ guiadev/base/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("", views.inicio_view, name="inicio"),
     path("inicio/", views.inicio_view, name="inicio"),
     path("index/", views.index_view, name="index"),
-    path("tutoriais/", views.tutoriais_view, name="tutoriais"),
-    path("tutorial/", views.tutorial_view, name="tutorial"),
     path("login/", views.login_view, name="login"),
     path('register/', views.register_view, name='register'),
+    path("tutoriais/", views.tutoriais_view, name="tutoriais"),
+    path("tutorial/", views.tutorial_view, name="tutorial"),
     path("adicionar_tutorial/", views.adicionar_tutorial_view, name="adicionar_tutorial"),
 ]
